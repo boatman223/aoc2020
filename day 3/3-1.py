db21e7cd3293a253064d6c1e0107e7ce3cfc3d06
boatman223 <jmquander@gmail.com>
2020-12-03 13:33:22 -0500

    day 3

---
+++ day 3/3-1.py
@@ -0,0 +1,47 @@
+from collections import namedtuple
+import math
+
+with open('input') as f:
+    world_map = [i.strip() for i in f.readlines()]
+
+Slope = namedtuple('Slope', ['x', 'y'])
+
+slope = Slope(3, 1)
+
+def expand_map(world_map, slope):
+    full_map = []
+    height = len(world_map)
+    width = len(world_map[0])
+    req_width = ((slope.x / slope.y) * height)
+    exp_factor = int(math.ceil(req_width / width))
+    for row in world_map:
+        full_map.append(row * exp_factor)
+    return full_map
+
+def locate_trees(full_map):
+    trees = set()
+    for y, row in enumerate(full_map):
+        for x, tile in enumerate(row):
+            if tile == '#':
+                trees.add((x, y))
+    return trees
+
+def traverse_map(trees, slope):
+    height = len(full_map)
+    width = len(full_map[0])
+    x = 0
+    y = 0
+    tree_count = 0
+    while y <= height:
+        x += slope.x
+        y += slope.y
+        if (x, y) in trees:
+            tree_count += 1
+    return tree_count
+
+
+full_map = expand_map(world_map, slope)
+trees = locate_trees(full_map)
+tree_count = traverse_map(trees, slope)
+
+print(tree_count)
