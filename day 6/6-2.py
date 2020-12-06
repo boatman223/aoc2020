b096aac5e4ee6264109699f675ef13c72da35863
boatman223 <jmquander@gmail.com>
2020-12-06 00:36:33 -0500

    day 6

--- day 6/6-2.py
+++ day 6/6-2.py
@@ -4,7 +4,7 @@ with open('input') as f:
 total = 0
 for group in groups:
     answers = [set(i) for i in group]
-    intersection = answers[0].intersection(*answers[1:])
+    intersection = set.intersection(*answers)
     total += len(intersection)
 
 print(total)
