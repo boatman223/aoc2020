1192937ae6adc25a1b422f221925053435472ac2
boatman223 <jmquander@gmail.com>
2020-12-15 13:07:34 -0500

    day 15

---
+++ day 15/15-2.py
@@ -0,0 +1,18 @@
+import collections
+
+with open('input') as f:
+    numbers = [int(x) for x in f.read().split(',')[:-1]]
+
+numberdict = collections.defaultdict(int, {v:k+1 for k,v in enumerate(numbers)})
+timestep = 6
+current = 0
+while timestep < 30000000:
+    lasttime = numberdict[current]
+    numberdict[current] = timestep
+    if lasttime:
+        current = timestep - lasttime
+    else:
+        current = 0
+    timestep += 1
+
+print(current)
