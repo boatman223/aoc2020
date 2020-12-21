b83e835a8c31f079548d1630d5a1ee9f21164ab8
boatman223 <jmquander@gmail.com>
2020-12-21 05:05:28 -0500

    day 20

--- day 20/20-2.py
+++ day 20/20-2.py
@@ -1,5 +1,6 @@
 import regex
 import random
+from typing import Iterator
 
 class Tile:
 
@@ -11,18 +12,18 @@ class Tile:
             out += f'{row}\n'
         return out
 
-    def __init__(self, tile):
+    def __init__(self, tile: list[str]):
         self.id = int(tile[1][:-1])
         self.tile = tile[2:]
-        self.coordinates = []
+        self.coords = []
 
-    def row(self, num):
+    def row(self, num: int) -> str:
         return self.tile[num]
 
-    def col(self, num):
+    def col(self, num: int) -> str:
         return ''.join([x[num] for x in self.tile])
 
-    def edges(self):
+    def edges(self) -> Iterator[tuple[int, str]]:
         edges = {
             0:   self.tile[0],
             90:  ''.join([x[-1] for x in self.tile]),
@@ -32,23 +33,23 @@ class Tile:
         for angle, edge in edges.items():
             yield angle, edge
 
-    def rotate90(self):
+    def rotate90(self) -> list[str]:
         new_tile = [self.col(i)[::-1] for i in range(10)]
         return new_tile
 
-    def flip_h(self):
+    def flip_h(self) -> list[str]:
         new_tile = [self.row(i)[::-1] for i in range(10)]
         return new_tile
 
-    def flip_v(self):
+    def flip_v(self) -> list[str]:
         new_tile = [self.row(i) for i in reversed(range(10))]
         return new_tile
 
-    def strip_borders(self):
+    def strip_borders(self) -> list[str]:
         new_tile = [row[1:-1] for row in self.tile[1:-1]]
         return new_tile
 
-def orient_tiles(tiles, starting_tile):
+def orient_tiles(tiles: list[Tile], starting_tile: Tile):
     normal_match = {
         0:  {
             0:   ('flip_v',),
@@ -96,64 +97,59 @@ def orient_tiles(tiles, starting_tile):
                         match = normal_match if edge == edge2 else flipped_match
                         for op in match[a1][a2]:
                             tile.tile = getattr(tile, op)()
-                        tile.coordinates = [coords[angle][0]+center.coordinates[0], coords[angle][1]+center.coordinates[1]]
+                        tile.coords = [coords[angle][0]+center.coords[0], coords[angle][1]+center.coords[1]]
                         tiles.remove(tile)
                         queue.append(tile)
 
-def adjust_origin(tiles):
-    min_x = 0
-    max_y = 0
+def build_image(tiles: list[Tile]) -> list[str]:
+    y_adj = 0
     for tile in tiles:
-        min_x = min(min_x, tile.coordinates[0])
-        max_y = max(max_y, tile.coordinates[1])
-    for tile in tiles:
-        tile.coordinates[0] -= min_x
-        tile.coordinates[1] -= max_y
-        tile.coordinates[1] = abs(tile.coordinates[1])
-
-def build_image(tiles):
+        y_adj = max(y_adj, tile.coords[1])
     image = []
-    for tile in sorted(tiles, key=lambda x: (x.coordinates[1], x.coordinates[0])):
+    for tile in sorted(tiles, key=lambda x: (abs(x.coords[1]-y_adj), x.coords[0])):
+        tile.coords[1] = abs(tile.coords[1]-y_adj)
         stripped_tile = tile.strip_borders()
-        for i in range(8):
-            try: image[tile.coordinates[1]*8+i] += stripped_tile[i]
-            except: image.append(stripped_tile[i])
+        width = len(stripped_tile[0])
+        for i in range(width):
+            try: image[tile.coords[1]*width+i] += stripped_tile[i]
+            except IndexError: image.append(stripped_tile[i])
     return image
 
-def find_monsters(image):
+def find_monsters(image: list[str]) -> tuple[list[str], int]:
 
-    def _flip_image(image):
+    def _flip_image(image: list[str]) -> list[str]:
         new_image = [line[::-1] for line in image]
         return new_image
 
-    def _rotate_image(image):
+    def _rotate_image(image: list[str]) -> list[str]:
         new_image = []
         for i in range(len(image[0])):
             new_image.append(''.join([x[i] for x in image])[::-1])
         return new_image
 
+    def _all_orientations(image: list[str]) -> Iterator[list[str]]:
+        for _ in range(2):
+            for _ in range(4):
+                yield image
+                image = _rotate_image(image)
+            image = _flip_image(image)
+
     MONSTER1 = regex.compile('..................#.')
     MONSTER2 = regex.compile('#....##....##....###')
     MONSTER3 = regex.compile('.#..#..#..#..#..#...')
     monsters = 0
-    rotates = 0
-    while not monsters:
-        for row, line in enumerate(image):
+    for orientation in _all_orientations(image):
+        for row, line in enumerate(orientation):
             if matches := regex.finditer(MONSTER3, line, overlapped=True):
                 for match in matches:
-                    if regex.match(MONSTER2, image[row-1][match.start():match.end()]):
-                        if regex.match(MONSTER1, image[row-2][match.start():match.end()]):
+                    if regex.match(MONSTER2, orientation[row-1][match.start():match.end()]):
+                        if regex.match(MONSTER1, orientation[row-2][match.start():match.end()]):
                             monsters += 1
-        image = _rotate_image(image)
-        rotates += 1
-        if rotates == 4: image = _flip_image(image)
-        if rotates == 8: break
+        if monsters: break
     return image, monsters
 
-def get_solution(image, monsters):
-    total = 0
-    for line in image:
-        total += line.count('#')
+def get_solution(image: list[str], monsters: int) -> int:
+    total = sum((line.count('#') for line in image))
     total -= monsters*15
     return total
 
@@ -162,10 +158,9 @@ with open('input') as f:
 
 Tile.tiles = [Tile(tile) for tile in tiles]
 starting_tile = Tile.tiles[random.randint(0, len(Tile.tiles)-1)]
-starting_tile.coordinates = [0, 0]
+starting_tile.coords = [0, 0]
 
 orient_tiles(Tile.tiles[:], starting_tile)
-adjust_origin(Tile.tiles)
 image = build_image(Tile.tiles)
 image, monsters = find_monsters(image)
 solution = get_solution(image, monsters)
